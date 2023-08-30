a24a8a398e2f403af237932a13a83e7a6b9e5d98
Luis Lyra <luis.lyra@pitang.com>
2023-08-30 09:02:55 -0300

    adjusting css

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -7,7 +7,7 @@ function ExpenseItem(props) {
       <div>
         <ExpenseDate date={props.date} />
       </div>
-      <div className="expense=item__description">
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
